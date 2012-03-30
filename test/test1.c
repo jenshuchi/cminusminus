b8e49f6ec9baacda286beda469c6556bdc98883e
hong-jen kao <endless-dreaming@hotmail.com>
2012-03-30 23:23:11 +0800

    新增測試程式

---
+++ test/test1.c
@@ -0,0 +1,19 @@
+int main()
+{
+  /* 1: this is just a sample */
+  int n;
+  int abc, def, main_1;
+  struct new {
+    int a1;
+    float b1, z_123_x_45;
+  } s1;
+  write("Enter a number");
+
+  /* 2: this is a 3-line
+     comment
+  */
+  n = read();
+  abc = n + 1;
+  def = abc * abc;
+  write(def); /* 3: this is /* the end ***/
+}
